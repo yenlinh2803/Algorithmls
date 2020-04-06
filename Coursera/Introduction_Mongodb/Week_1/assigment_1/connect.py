92d6aedf714b9fc6ae76d34c8a634b91c1903fcf
yenlinh2803 <tnyenlinh@gmail.com>
2020-04-06 11:17:27 +0700

    commit restructure

--- Coursera/Introduction_Mongodb/Week_1/assigment_1/connect.py
+++
@@ -1,6 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient("mongodb+srv://analytics:analytics-password@mflix-vfdsr.mongodb.net/test?retryWrites=true&w=majority")
-
-
-print(client.mflix)
